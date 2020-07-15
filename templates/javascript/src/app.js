b4a9f447213d8b552abc02bf16eee843644ffccd
Benjamin <benjamin@soltivo.com>
2020-07-15 11:07:36 -0400

    path issue fixed on the template

--- templates/javascript/src/app.js
+++ templates/javascript/src/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-var { router } = require('./src/routes/index');
+var { router } = require('./routes/index');
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 // parse requests of content-type: application/x-www-form-urlencoded
