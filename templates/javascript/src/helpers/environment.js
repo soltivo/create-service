7e6397d54137e78889bb4aca9e71bed04ae25b8c
Benjamin Filiatrault <benjamin@soltivo.com>
2020-10-08 19:00:25 -0400

    added the latest environment loading

--- templates/javascript/src/helpers/environment.js
+++ templates/javascript/src/helpers/environment.js
@@ -1,69 +1,71 @@
 exports.load = async () => {
-    // Load .env file if exists
-    require('dotenv').config();
+	// Load .env file if exists
+	require('dotenv').config();
 
-    const region = process.env.AWS_REGION || 'us-east-1';
-    global.AWS = {};
-    global.STRIPE = {};
+	const region = process.env.AWS_REGION || 'us-east-1';
+	global.AWS = {};
+	global.STRIPE = {};
 
-    // On ECS, secrets are set in the container environment from the SecretsManager
-    if (this.isStageEnvironment()) {
-        const errors = [];
-        if(!process.env.aws_secret) {
-            errors.push('AWS secrets not found in environment (aws_secret)');
-        }
-        if(!process.env.stripe_secret) {
-            errors.push('Stripe secrets not found in environemnt (stripe_secret)');
-        }
-        if(errors.length != 0) {
-            throw new Error(errors);
-        }
+	try {
+		// On ECS, secrets are set in the container environment from the SecretsManager
+		if (this.isStageEnvironment()) {
+			const errors = [];
+			if (!process.env.aws_secret) {
+				errors.push('AWS secrets not found in environment (aws_secret)');
+			}
+			if (!process.env.stripe_secret) {
+				errors.push('Stripe secrets not found in environemnt (stripe_secret)');
+			}
+			if (errors.length != 0) {
+				throw new Error(errors);
+			}
 
-        // Set global variables
-        const aws_secrets = JSON.parse(process.env.aws_secret);
-        global.AWS.region = region;
-        global.AWS.secrets = aws_secrets;
-        
-        const stripe_secrets = JSON.parse(process.env.stripe_secret);
-        global.STRIPE.secrets = stripe_secrets;
+			// Set global variables
+			const aws_secrets = JSON.parse(process.env.aws_secret);
+			global.AWS.region = region;
+			global.AWS.secrets = aws_secrets;
 
-    } else {
+			const stripe_secrets = JSON.parse(process.env.stripe_secret);
+			global.STRIPE.secrets = stripe_secrets;
+		} else {
+			// On local we use dummy or need to use the Secrets Manager
+			if (this.isLocalEnvironment()) {
+				global.AWS.region = 'local';
+				global.AWS.secrets = {
+					AWS_ACCESS_KEY_ID: 'access-key-id', // don't touch
+					AWS_SECRET_ACCESS_KEY: 'secret-access-key', // don't touch
 
-        // On local we use dummy or need to use the Secrets Manager
-        if(this.isLocalEnvironment()) {
-            global.AWS.region = 'local.region';
-            global.AWS.secrets = {
-                AWS_ACCESS_KEY_ID: 'access-key-id', // don't touch
-                AWS_SECRET_ACCESS_KEY: 'secret-access-key', // don't touch
-                
-                API_LINK: 'http://localhost:3000' // Change that for the right link 
-            };
-            global.STRIPE.secrets = {
-                STRIPE_SECRET_KEY: 'secret_key',
-                STRIPE_PUBLIC_KEY: 'public_key',
-                STRIPE_CLIENT_ID: 'client_id'
-            }
-            
-        // If it is wanted to run local with stage environment
-        } else if(this.isLocalWithAWSEnvironment()) {
-            const secretsManager = require('./secretsManager');
-            await secretsManager.loadSecrets(region, 'dev/keys', 'AWS');
-            await secretsManager.loadSecrets(region, 'dev/stripe', 'STRIPE');
-            global.AWS.region = region;
-        }
-    }
-}
+					API_LINK: 'http://localhost:3000', // Change that for the right link
+				};
+				global.STRIPE.secrets = {
+					STRIPE_SECRET_KEY: 'secret_key',
+					STRIPE_PUBLIC_KEY: 'public_key',
+					STRIPE_CLIENT_ID: 'client_id',
+				};
+
+				// If it is wanted to run local with stage environment
+			} else if (this.isLocalWithAWSEnvironment()) {
+				const secretsManager = require('./secretsManager');
+				await secretsManager.loadSecrets(region, 'dev/keys', 'AWS');
+				await secretsManager.loadSecrets(region, 'dev/stripe', 'STRIPE');
+				global.AWS.region = region;
+			}
+		}
+	} catch (error) {
+		throw new Error(error);
+	}
+};
 
 exports.isLocalEnvironment = () => {
-    return !process.env.ENVIRONMENT || process.env.ENVIRONMENT == 'local';
-}
+	return !process.env.ENVIRONMENT || process.env.ENVIRONMENT == 'local';
+};
 // To run locally with AWS services
 exports.isLocalWithAWSEnvironment = () => {
-    return process.env.ENVIRONMENT == 'local_AWS';
-}
+	return process.env.ENVIRONMENT == 'local_AWS';
+};
 exports.isStageEnvironment = () => {
-    return process.env.ENVIRONMENT == 'stage';
-}
+	return process.env.ENVIRONMENT == 'stage';
+};
 exports.isProdEnvironment = () => {
-    return process.env.ENVIRONMENT == 'prod';
-}
+	return process.env.ENVIRONMENT == 'prod';
+};
--- templates/javascript/src/helpers/secretsManager.js
+++ templates/javascript/src/helpers/secretsManager.js
@@ -1,37 +1,39 @@
 const AWS = require('aws-sdk');
 
 let loadSecrets = async (region, secretName, id) => {
-    // Create a Secrets Manager client
-    const client = new AWS.SecretsManager({
-        region: region
-    });
+	// Create a Secrets Manager client
+	const client = new AWS.SecretsManager({
+		region: region,
+	});
 
-    // Add key if not present
-    if(!global[id]) {
-        global[id] = {};
-    }
-    
-    // Check if already loaded
-    if(global[id] && global[id].secrets) {
-        return global[id].secrets;
-    }
+	// Add key if not present
+	if (!global[id]) {
+		global[id] = {};
+	}
 
-    let secrets;
-    await client.getSecretValue({ SecretId: secretName }).promise()
-        .then(function (data) {
-            if ('SecretString' in data) {
-                secrets = data.SecretString;
-            } else {
-                let buff = new Buffer(data.SecretBinary, 'base64');
-                secrets = buff.toString('ascii');
-            }
-        })
-        .catch(function (err) {
-            throw err;
-        });
+	// Check if already loaded
+	if (global[id] && global[id].secrets) {
+		return global[id].secrets;
+	}
 
-    global[id].secrets = JSON.parse(secrets);
-    return global[id].secrets;
-}
+	let secrets;
+	await client
+		.getSecretValue({ SecretId: secretName })
+		.promise()
+		.then(function (data) {
+			if ('SecretString' in data) {
+				secrets = data.SecretString;
+			} else {
+				let buff = new Buffer(data.SecretBinary, 'base64');
+				secrets = buff.toString('ascii');
+			}
+		})
+		.catch(function (err) {
+			throw err;
+		});
 
-module.exports = { loadSecrets }
+	global[id].secrets = JSON.parse(secrets);
+	return global[id].secrets;
+};
+
+module.exports = { loadSecrets };
